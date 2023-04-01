f45e1aca8879334c0a9c5ec9f5c082050a3485be
Cissé <tairou.cisse@axazara.com>
2023-04-01 22:01:22 +0100

    - handle event

--- src/App.js
+++ src/App.js
@@ -5,11 +5,29 @@ import { MdDone, MdClose } from "react-icons/md";
 export default function App() {
   const [password, setPassword] = useState(false);
   const [passwordConfirm, setPasswordConfirm] = useState(false);
+  const [showPasswordChecklist, setShowPasswordChecklist] = useState(true);
   // console.log(password);
   // console.log(passwordConfirm);
-
+  
+// cacher le PasswordChecklist lorsque l'utilisateur à insérer tout les caractères qu'il faut pour définir son mot de passe
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value.length >= 8) {
+      setShowPasswordChecklist(false);
+    } else {
+      setShowPasswordChecklist(true);
+    }
+  };
+  // handlePasswordFocus lorsque l'utilisateur clique sur l'input
+  const handlePasswordFocus = () => {
+    setShowPasswordChecklist(true);
+  };
+// handlePasswordBlur si l'user clique hors de l'input le PasswordCheckList passe au false
+  const handlePasswordBlur = () => {
+    setShowPasswordChecklist(false);
+  };
   return (
-    <div className="w-96 mx-auto flex justify-center items-center pt-20">
+    <div className="w-full mx-auto flex justify-center items-center pt-20">
       <form className="space-y-5">
         <div className="flex flex-col">
           <label className="text-semibold">Password</label>
@@ -17,7 +35,10 @@ export default function App() {
             type="password"
             placeholder="Entrer votre mot de passe"
             className="border-2 border-black rounded-md py-1 text-xl focus:outline-none p-2"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={handlePasswordChange}
+            onFocus={handlePasswordFocus}
+            onBlur={handlePasswordBlur}
+            // onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="flex flex-col">
@@ -29,23 +50,25 @@ export default function App() {
             onChange={(e) => setPasswordConfirm(e.target.value)}
           />
         </div>
-        <PasswordChecklist
-          rules={["number", "capital", "specialChar", "lowercase"]}
-          minLength={10}
-          value={password}
-          valueAgain={passwordConfirm}
-          className="text-xs font-bold"
-          iconComponents={{
-            ValidIcon: <MdDone size={20} className="text-green-500" />,
-            InvalidIcon: <MdClose size={20} className="text-red-500" />,
-          }}
-          messages={{
-            number: "doit comporter des nombres",
-            capital: "doit comporter des nombres majuscules",
-            specialChar: "doit comporter des caractères spéciaux",
-            lowercase: "doit comporter des minuscules",
-          }}
-        />
+        {showPasswordChecklist && (
+          <PasswordChecklist
+            rules={["number", "capital", "specialChar", "lowercase"]}
+            minLength={10}
+            value={password}
+            valueAgain={passwordConfirm}
+            className="text-xs font-bold"
+            iconComponents={{
+              ValidIcon: <MdDone size={20} className="text-green-500" />,
+              InvalidIcon: <MdClose size={20} className="text-red-500" />,
+            }}
+            messages={{
+              number: "doit comporter des nombres",
+              capital: "doit comporter des nombres majuscules",
+              specialChar: "doit comporter des caractères spéciaux",
+              lowercase: "doit comporter des minuscules",
+            }}
+          />
+        )}
       </form>
     </div>
   );
