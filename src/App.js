2b0b2acf46e509261f16db595a6d7f5d0165e6a8
Cissé <tairou.cisse@axazara.com>
2023-04-01 10:04:07 +0100

    add form and password verifications condition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PasswordChecklist from "react-password-checklist";
+import { MdDone, MdClose } from "react-icons/md";
 
-function App() {
+export default function App() {
+  const [password, setPassword] = useState(false);
+  const [passwordConfirm, setPasswordConfirm] = useState(false);
+  // console.log(password);
+  // console.log(passwordConfirm);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-96 mx-auto flex justify-center items-center">
+      <form className="space-y-5">
+        <div className="flex flex-col">
+          <label>Password</label>
+          <input
+            type="Name"
+            className="border-2 border-lime-600 rounded-md py-1 text-2xl focus:outline-none p-2"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label>Confirm password</label>
+          <input
+            type="password"
+            className="border-2 border-lime-600 rounded-md py-1 text-2xl focus:outline-none p-2"
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+          />
+        </div>
+        <PasswordChecklist
+          rules={["number", "capital", "specialChar", "lowercase"]}
+          minLength={10}
+          value={password}
+          valueAgain={passwordConfirm}
+          className="text-xs text-black"
+          iconComponents={{
+            ValidIcon: <MdDone size={15} />,
+            InvalidIcon: <MdClose size={15} />,
+          }}
+          messages={{
+            number: "doit comporter des nombres",
+            capital: "doit comporter des nombres majuscules",
+            specialChar: "doit comporter des caractères spéciaux",
+            lowercase: "doit comporter des minuscules",
+          }}
+        />
+      </form>
     </div>
   );
 }
-
-export default App;
